6d4f2399a7c02731637e8122deac3672f2e90cef
suhanibedi <sbed0010@student.monash.edu>
2025-05-31 16:36:48 +1000

    Update hashy_step_table.py

--- hashy_step_table.py
+++ hashy_step_table.py
@@ -16,6 +16,8 @@ V = TypeVar('V')
 class FullError(Exception):
     pass
 
+class Sentinel():
+    pass
 
 class HashyStepTable(Generic[K, V]):
     """
@@ -69,10 +71,11 @@ class HashyStepTable(Generic[K, V]):
         Used to determine the step size for our hash table.
 
         Complexity:
-        Best Case Complexity:
-        Worst Case Complexity:
+        Best Case Complexity: O(1)
+        Worst Case Complexity: O(1)
         """
-        raise NotImplementedError
+        value = (4*len(key)*ord(key[0]) + 3) % 13
+        return value
 
     @property
     def table_size(self) -> int:
@@ -93,14 +96,46 @@ class HashyStepTable(Generic[K, V]):
         FullError: When a table is full and cannot be inserted.
 
         Complexity:
-        Best Case Complexity:
-        Worst Case Complexity:
+        Best Case Complexity: O(k), where k is the len(key)
+        Worst Case Complexity: O(k + N), where k is the len(key) and N is self.table_size
+
+        Complexity Analysis:
+        The best case scenario occurs when the initial position is determined, and in the first iteration of the
+        for loop, self.array[position] is None and the is_insert argument is True. This allows the loop to terminate
+        early, as the final position is quickly returned. Therefore, making the best case complexity O(k), where k is the len(key).
+
+        The worst case scenario occurs when the initial position is determined, and the for loop has to run through the capacity 
+        of the hash table (self.table_size) because the key being search for is not in the hash table. This means the is_insert
+        argument is False, and the key is searched in the list outputted by self.keys() to then raise a keyerror. Hence, this makes the
+        worst case complexity simplified to O(k + N)), where k is the len(key) and N is self.table_size.
         """
         # Initial position
         position = self.hash(key)
 
         # Custom logic to be implemented here
-        raise NotImplementedError
+        step_size = self.hash2(key)
+
+        for _ in range(0,self.table_size):
+            if self.array[position] is None:
+                if is_insert:
+                    return position
+                else:
+                    position = (position + step_size) % self.table_size
+                    continue
+            elif self.array[position] is Sentinel:
+                if is_insert:
+                    return position
+            elif self.array[position][0] == key:
+                return position
+            else:
+                position = (position + step_size) % self.table_size
+
+        if is_insert:
+            raise FullError("Table is full!")
+        else:
+            if key not in self.keys():
+                raise KeyError(key)
+
     
     def keys(self) -> list[K]:
         """
@@ -156,7 +191,6 @@ class HashyStepTable(Generic[K, V]):
         :complexity: See hashy probe.
         :raises FullError: when the table cannot be resized further.
         """
-
         position = self._hashy_probe(key, True)
 
         if self.array[position] is None:
@@ -172,10 +206,27 @@ class HashyStepTable(Generic[K, V]):
         Deletes a (key, value) using lazy deletion
 
         Complexity:
-        Best Case Complexity:
-        Worst Case Complexity:
+        Best Case Complexity: O(1)
+        Worst Case Complexity: O(N), where N is self.table_size.
+
+        Complexity analysis:
+        The best case occurs when the value of the first element of self.array corresponds to the 
+        key argument. This allows the loop to terminate early, hence the best case complexity being constant.
+
+        The worst case occurs when the value of the last element of self.array corresponds to the key argument.
+        This means the loop runs through all elements in self.array, meaning N times, hence the worst case
+        complexity being O(N), where N is self.table_size.
         """
-        raise NotImplementedError
+        position = None
+        for elem in range(len(self.array)):
+            if self.array[elem] is None:
+                continue
+            if self.array[elem][0] == key:
+                position = elem
+                break
+        if position is not None:
+            self.array[position] = (Sentinel, Sentinel)
+        self.count -= 1
 
     def is_empty(self) -> bool:
         return self.count == 0
@@ -188,10 +239,29 @@ class HashyStepTable(Generic[K, V]):
         Need to resize table and reinsert all values
 
         Complexity:
-        Best Case Complexity:
-        Worst Case Complexity:
+        Best Case Complexity: O(l + N)), where l is the number of elements in self.TABLE_SIZES, and N is self.table_size 
+        Worst Case Complexity: O(l + N)), where l is the number of elements in self.TABLE_SIZES and N is self.table_size
+
+        Complexity analysis:
+        The best case and worst case scenario occurs when the index of self.table_size is found from the 
+        self.TABLE_SIZES list in HashyStepTable class, which has a time complexity of O(l), where l is the 
+        number of elements in self.TABLE_SIZES. To add all the elements in the current hash table into the new hash 
+        table, the for loop iterates through all current elements, which has a time complexity of O(N), where N is
+        self.table_size. Hence, the best case and worst case complexity is when these two complexities are added, so
+        it is expressed as O(l + N), where l is the number of elements in self.TABLE_SIZES, and N is self.table_size. 
+
         """
-        raise NotImplementedError
+        i = self.TABLE_SIZES.index(self.table_size)
+        old_table = self.array
+        self.table = ArrayR(self.TABLE_SIZES[i + 1])
+        # Empty
+        x = 0
+        for item in old_table:
+            if item is not None:
+                # Insert
+                self.table[x] = (item[0],item[1])
+            x += 1
+        self.array = self.table
 
     def __str__(self) -> str:
         """
